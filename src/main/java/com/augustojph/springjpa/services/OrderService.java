36f28e676179b4b31f5ed26d90fb16d1a5b84189
José Augusto <Augusto2599@outlook.com>
2024-06-04 16:11:28 -0300

    Revert "Revert "Configurando o pacote Service e tratando suas exeções.""
    This reverts commit d9d9f709096a06872dfd2fb8f80d67f320719e00.

---
+++ src/main/java/com/augustojph/springjpa/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.augustojph.springjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.augustojph.springjpa.entities.Order;
+import com.augustojph.springjpa.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
---
+++ src/main/java/com/augustojph/springjpa/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.augustojph.springjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.augustojph.springjpa.entities.Product;
+import com.augustojph.springjpa.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findAll(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
